c0ca5b40d583ef10efd68f371e6ae8148cec6128
kykint@gmail.com <kykint@gmail.com@e2b92cfd-bb87-d2de-68e3-549772af2248>
2012-11-14 15:55:49 +0000

    - history

--- trunk/web/res/all.js
+++ trunk/web/res/all.js
@@ -1,5 +1,37 @@
+var lastQuery = 0;
+if (!store.get("last"))
+	store.set("last", 1);
+function binding(){
+	/*$(document).bind('keydown', 'tab',function (evt){
+		alert(evt);
+		tab();
+		return false;
+	});*/
+	$(document).bind('keyup', 'up',function (evt){
+		last_queries(1);
+		return false;
+	});
+	$(document).bind('keydown', 'down',function (evt){
+		last_queries(0);
+		return false;
+	});
+}
+$(document).ready(binding);
+/*
+jQuery(document).bind('keyup', 'up',function (evt){
+	
+	return false;
+});
+jQuery(document).bind('keyup', 'down',function (evt){
+	last_queries(0);
+	return false;
+});*/
 function execute(){
 	var query = $("#query").val();
+	if (query){
+		storing(query);
+	}
+	
 	switch(query){
 		case "":
 			echo();
@@ -30,4 +62,31 @@ function echo(text, query){
 	if (!query) query="";	
 	$("#results").append("<div class='result'>>"+text+"</div>");
 	$("#query").val(query);
+}
+function tab(){
+	
+}
+function last_queries(updown){
+	if (!lastQuery){
+		lastQuery = Number(store.get("last"));
+	}
+	if (updown==1){
+		if (lastQuery==1) return;
+		lastQuery--;
+		$("#query").val(store.get("q["+lastQuery+"]"));
+	}else{
+		if (lastQuery >= Number(store.get("last"))) return;
+		lastQuery++;
+		if (lastQuery == Number(store.get("last")))
+			$("#query").val("");
+		else
+			$("#query").val(store.get("q["+lastQuery+"]"));
+	}
+}
+function storing(query){
+	var last = store.get("last");
+	last = Number(last);
+	store.set("q["+last+"]", query);
+	last++;
+	store.set("last", last);
 }
