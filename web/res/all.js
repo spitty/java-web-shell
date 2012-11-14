2b26441359f3d982adc52aee7243ffce6355d2d0
kykint@gmail.com <kykint@gmail.com@e2b92cfd-bb87-d2de-68e3-549772af2248>
2012-11-14 14:59:52 +0000

    - Added command "clear"

--- web/res/all.js
+++ web/res/all.js
@@ -1,21 +1,29 @@
 function execute(){
 	var query = $("#query").val();
-	if (!query) {
-		echo();
-		return;
+	switch(query){
+		case "":
+			echo();
+			break;
+		case "clear":
+			echo();
+			$("#results").html("");
+			break;
+		default:
+			$.ajax({
+				type : 'POST',
+				url : '/ShellPostProcessor',
+				cache : false,
+				data : {
+					query : query
+				},
+				error : function () {
+					echo("Connection error", query);
+				},
+				success : function (data) {
+					echo (data);
+				}
+			});
 	}
-	$.ajax({
-		type : 'POST',
-		url : '/ShellPostProcessor',
-		cache : false,
-		data : {query : query},
-		error : function () {
-			echo("Connection error", query);
-		},
-		success : function (data) {
-			echo (data);
-		}
-	});
 }
 function echo(text, query){
 	if (!text) text="";
